045b51062e8795e0eb3ca5d663db23c76fdae0b0
sosa7753 <sosa7753@naver.com>
2024-07-20 20:20:58 +0900

    섹션 7

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,20 +3,17 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // final에 대해 생성자 할당
 public class OrderServiceImpl implements OrderService {
 
   // final은 무조건 할당이 되어있어야함. 하나라도 빠지면 에러
   private final MemberRepository memberRepository;
   private final DiscountPolicy discountPolicy;
-
-  public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-    this.memberRepository = memberRepository;
-    this.discountPolicy = discountPolicy;
-  }
 
   // 이 구체화에 의존하지 않으려면 누군가 구현 객체를 대신 생성해서 주입해 줘야한다.
   @Override
@@ -25,10 +22,5 @@ public class OrderServiceImpl implements OrderService {
     int discountPrice = discountPolicy.discount(member, itemPrice);
 
     return new Order(memberId, itemName, itemPrice, discountPrice);
-  }
-
-  // 테스트 용도
-  public MemberRepository getMemberRepository() {
-    return memberRepository;
   }
 }
