5bae1cda7970b54874272fb2efc6adcf08ec4f46
sosa7753 <sosa7753@naver.com>
2024-07-20 17:49:50 +0900

    섹션 7

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -14,8 +14,6 @@ public class OrderServiceImpl implements OrderService {
   private final DiscountPolicy discountPolicy;
 
   public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-    System.out.println("memberRepository = " + memberRepository);
-    System.out.println("discountPolicy = " + discountPolicy);
     this.memberRepository = memberRepository;
     this.discountPolicy = discountPolicy;
   }
