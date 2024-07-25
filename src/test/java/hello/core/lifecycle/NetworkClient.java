193c9065badf3177103cb0b5a97de3d255c8e39c
sosa7753 <sosa7753@naver.com>
2024-07-25 20:21:20 +0900

    섹션 8

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,15 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+// 초기화, 소멸 메서드 단점 : 스프링 전용 인터페이스라 의존하게 됨.
+public class NetworkClient implements InitializingBean, DisposableBean {
 
   private String url;
 
   public NetworkClient() {
     System.out.println("생성자 호출, url = " + url);
-    connect();
-    call("초기화 연결 메세지");
   }
 
   public void setUrl(String url) {
@@ -26,5 +28,19 @@ public class NetworkClient {
   // 서비스 종료시 호출
   public void disconnect() {
     System.out.println("close: " + url);
+  }
+
+  // 의존관계 주입이 끝나면 호출
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    System.out.println("NetworkClient.afterPropertiesSet");
+    connect();
+    call("초기화 연결 메세지");
+  }
+
+  @Override
+  public void destroy() throws Exception {
+    System.out.println("NetworkClient.destroy");
+    disconnect();
   }
 }
